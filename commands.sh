16072bee152de3a4a185346f36962213113a4838
Yao-Chu Chiu <yao-chu.chiu@access-company.com>
2019-12-25 17:56:19 +0800

    Fix shell script

--- commands.sh
+++ commands.sh
@@ -1,8 +1,8 @@
 echo "run read_all_data_md.py"
-python3 read_all_data_md.py
+python3 read_all_data_md.py $1
 
 echo "run drafter to parse data apib to json"
 drafter -o save_by_drafter.json -f json temp.apib
 
-# echo "run readMD.py"
-# python3 readMD.py
+echo "run readMD.py"
+python3 readMD.py $2
